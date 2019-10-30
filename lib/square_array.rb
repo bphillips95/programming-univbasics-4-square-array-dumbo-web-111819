bc413e95e632b78248350099ff38845aeace2ffa
Baruch Phillips <baruchphillips@gmail.com>
2019-10-30 17:34:26 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
  counter = 0 
  
   while counter < array.length do
-    prints [array[counter]**array[counter]]
+    print [array[counter]**array[counter]]
     counter += 1
   end
     
